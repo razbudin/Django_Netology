b4c1bae80c3742e5ef550a14343d5cb7ced38d15
razbudin <razbudin@yandex.ru>
2024-07-29 18:32:08 +0300

    first commit

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import datetime
+import os
 
 
 def home_view(request):
@@ -8,10 +10,10 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -21,15 +23,24 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    template_name = 'app/time.html'
+    current_time = datetime.datetime.now().time()
+    context = {
+        'time': current_time.strftime("%H:%M:%S")
+    }
+    return render(request, template_name, context)
 
 
 def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
+    # который возвращает список файлов в рабочей
     # директории
+    template_name = 'app/workdir.html'
+    workdir = os.listdir(path='.')
+    context = {
+        'workdir': workdir
+    }
+    return render(request, template_name, context)
     raise NotImplemented
